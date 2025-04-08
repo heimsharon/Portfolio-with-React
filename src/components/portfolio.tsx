2d446989286cff3a55f12956f59c8d045286c4b8
heimsharon <heimsharon@hotmail.com>
2025-04-08 01:03:48 -0400

    more port updates

--- src/components/portfolio.tsx
+++ src/components/portfolio.tsx
@@ -12,7 +12,7 @@ const Portfolio: React.FC = () => {
       <div className="portfolio__item weather-dashboard">
           <h3>Weather Dashboard</h3>
           <a href="https://github.com/heimsharon/Weather-Dashboard-API-Usage" target="_blank" rel="noopener noreferrer" className="github-link">
-            <img src="./assets/images/github-logo5.png" alt="GitHub Logo" className="github-icon" />
+            <img src="./assets/images/github-mark.png" alt="GitHub Logo" className="github-icon" />
           </a>
           <a href="https://weather-api-mod-9.onrender.com/" target="_blank" rel="noopener noreferrer" className="deployed-link">
             View Deployed Project
@@ -31,12 +31,12 @@ const Portfolio: React.FC = () => {
         </div>
         
         
-        <div className="portfolio__item vehicle-builder">
-          <h3>Vehicle Builder</h3>
-          <a href="https://github.com/heimsharon/Employee-Tracker-Database-using-CMS" target="_blank" rel="noopener noreferrer" className="github-link">
-          <img src="./assets/images/github-mark-white.png" alt="GitHub Logo" className="github-icon" />
+        <div className="portfolio__item portfolio-early">
+          <h3>Portfolio Early</h3>
+          <a href="https://github.com/heimsharon/Portfolio-Early" target="_blank" rel="noopener noreferrer" className="github-link">
+          <img src="./assets/images/github-mark.png" alt="GitHub Logo" className="github-icon" />
           </a>
-          <a href="https://example-deployed-link.com" target="_blank" rel="noopener noreferrer" className="deployed-link">
+          <a href="https://heimsharon.github.io/Portfolio-Early/#" target="_blank" rel="noopener noreferrer" className="deployed-link">
             View Deployed Project
           </a>
         </div>
@@ -54,11 +54,11 @@ const Portfolio: React.FC = () => {
        
        
         <div className="portfolio__item seo-optimization">
-          <h3>Surf Report</h3>
-          <a href="https://github.com/heimsharon/Surf-Report" target="_blank" rel="noopener noreferrer" className="github-link">
-          <img src="./assets/images/github-mark-white.png" alt="GitHub Logo" className="github-icon" />
+          <h3>HTML SEO</h3>
+          <a href="https://github.com/heimsharon/SEO-HTML" target="_blank" rel="noopener noreferrer" className="github-link">
+          <img src="./assets/images/github-mark.png" alt="GitHub Logo" className="github-icon" />
           </a>
-          <a href="https://example-deployed-link.com" target="_blank" rel="noopener noreferrer" className="deployed-link">
+          <a href="https://heimsharon.github.io/SEO-HTML/" target="_blank" rel="noopener noreferrer" className="deployed-link">
             View Deployed Project
           </a>
         </div>
